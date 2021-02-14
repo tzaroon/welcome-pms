fdbd3dec08843d33939a10baf05a111aec705c06
Abrar Haq <abrar.ul.haq87@gmail.com>
2021-02-14 14:14:59 +0530

    -- CORs error

---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class Cors
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $handle = $next($request);
+
+        
+        if(method_exists($handle, 'header'))
+        {
+            $handle
+                ->header('Access-Control-Allow-Origin', "*")
+                ->header('Access-Control-Allow-Methods', "PUT,POST,DELETE,GET,OPTION")
+                ->header('Access-Control-Allow-Headers', "Accept,Authorization,Content-Type");
+        } else {
+            $handle->headers->set('Access-Control-Allow-Origin', "*");
+            $handle->headers->set('Access-Control-Allow-Methods', "PUT,POST,DELETE,GET,OPTION");
+            $handle->headers->set('Access-Control-Allow-Headers', "Accept,Authorization,Content-Type");
+        }
+
+        return $handle;
+    }
+}
