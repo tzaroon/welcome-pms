6798775a1d23c7dc9182870d0e908f5b7b59cb79
Abrar Haq <abrar.ul.haq87@gmail.com>
2021-02-19 22:53:31 +0530

    -- Hotel Add API

--- app/Http/Controllers/Api/V1/Hotels/HotelsController.php
+++ app/Http/Controllers/Api/V1/Hotels/HotelsController.php
@@ -28,4 +28,48 @@ class HotelsController extends Controller
 
         return response()->json($data);
     }
+
+    /**
+     * Store a new resource.
+     *
+     * @param Illuminate\Http\Request $request
+     *
+     * @return Illuminate\Http\JsonResponse
+     */
+    public function store(Request $request) : JsonResponse
+    {
+        $user = auth()->user();
+        
+        $postData = $request->getContent();
+        
+        $postData = json_decode($postData, true);
+
+        $validator = Validator::make($postData, [
+            'name' => 'required|string|max:191',
+            'property' => 'required|string|max:191',
+            'address' => 'required|string',
+            'zip' => 'required|string',
+            'state_id' => 'required',
+            'country_id' => 'required',
+            'phone' => 'required',
+            'email' => 'required|email',
+            'currency_id' => 'required'
+        ], [], [
+            'state_id' => 'State',
+            'country_id' => 'Country',
+            'currency_id' => 'Currency'
+        ]);
+
+        if (!$validator->passes()) {
+
+            return response()->json(array('errors' => $validator->errors()->getMessages()), 422);
+        }
+
+        $hotel = new Hotel();
+        $hotel->fill($postData);
+        $hotel->company_id = $user->company_id;
+        $hotel->save();
+
+        return response()->json($hotel);
+    }
 }
