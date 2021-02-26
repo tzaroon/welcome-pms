836dbef3102d566813181c443ce2eda174aa1549
Abrar Haq <abrar.ul.haq87@gmail.com>
2021-02-26 12:21:14 +0530

    -- Add ocupancy

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -8,8 +8,17 @@ class Room extends Model
 {
     protected $guarded = ['company_id'];
 
+    protected $appends = [
+        'ocupancy'
+    ];
+
     public function roomType() {
 
         return $this->belongsTo(RoomType::class);
+    }
+    
+    public function getOcupancyAttribute() {
+
+        return '1-6';
     }
 }
