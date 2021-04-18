08b27055a674f43683b0bf152ef183d9a095b1f8
Abrar Haq <abrar.ul.haq87@gmail.com>
2021-04-18 07:10:35 +0530

    -- WuBook API fix

--- app/Console/Commands/SyncWuBook.php
+++ app/Console/Commands/SyncWuBook.php
@@ -11,21 +11,21 @@ use App\Models\DailyPrice;
 use Carbon\Carbon;
 use App\Models\Hotel;
 
-class wooBook extends Command
+class SyncWuBook extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'wooBook';
+    protected $signature = 'sync:wubook';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Send data to woobook.';
+    protected $description = 'Send data to wubook.';
 
     /**
      * Create a new command instance.
@@ -46,12 +46,22 @@ class wooBook extends Command
     {
         $token = WuBook::auth()->acquire_token();
         $rooms = WuBook::rooms($token)->fetch_rooms();
-        $companyId = 1;     
-        //$plan = WuBook::prices($token)->add_pricing_plan('daily2', 1);        
-        //$planId = $plan['data']; 
-        $planId = 182099;
+        $companyId = 1;
+       /*  $plan = WuBook::prices($token)->add_pricing_plan('daily', 1);        
+        $planId = $plan['data'];
+        dd($planId);  */
+        $planId = 182114;
        
         $hotels = Hotel::where('company_id', $companyId)->whereNotNull('l_code')->get();
+
+        $dfrom =  Carbon::now();    
+        
+        $fromDateYmd = $dfrom->format('Y-m-d');   
+
+        $dfromdmY = $dfrom->format('d/m/Y');  
+        
+        $toDate = $dfrom->add('day', 999);                
+        $toDate = $toDate->format('Y-m-d');
 
         foreach($hotels as $hotel)
         {
@@ -74,39 +84,32 @@ class wooBook extends Command
 
                 if($room['subroom'] > 0)
                 {
-                    $rateType = RateType::where('company_id',  $companyId)->with(
-                        [                    
-                            'details' => function($q) use ($room) {
-                                $q->where('name', $room['name']);                            
-                            }
-                        ]
-                    )->get()->first();
+                    $rateType = RateType::where('company_id',  $companyId)->whereHas( 'details', 
+                        function($q) use ($room) {
+                            $q->where('name', $room['name']);                            
+                        }
+                    )->first();
 
                     if($rateType) 
                     {
                         $rateType->ref_id = $room['id'];                    
                         $rateType->save();     
                         
-                        $dfrom =  Carbon::now();                    
-                        $dfrom = $dfrom->format('d/m/Y');                  
-                    
-                        $dailyPrices = DailyPrice::where('company_id', $companyId)->get();
-                        $i =0;      
+                        $dailyPrices = DailyPrice::where('company_id', $companyId)
+                            ->where('rate_type_id', $rateType->id)
+                            ->where('date', '>=', $fromDateYmd)
+                            ->where('date', '<=', $toDate)->get();
 
+                        $i = 0;
                         foreach($dailyPrices as $dailyPrice)
                         {                                       
-                            $prices[$room['id']][$i] = $dailyPrice->product->price->price;                                                 
-                
-                            if($i == 365)
-                            {
-                                break;
-                            }
-                            $i++;
+                            $prices[$room['id']][$i] = $dailyPrice->product->price->price;       
+                            $i++;                                          
                         }                
                     }                
                 }         
-            }     
-            $result = WuBook::prices($token)->update_plan_prices($planId, $dfrom, $prices);
+            }
+            $result = WuBook::prices($token)->update_plan_prices($planId, $dfromdmY, $prices);
         }      
     }
 }
