75a30f67180bb5a03bb2c23ca085e36419ca4d3b
Abrar Haq <abrar.ul.haq87@gmail.com>
2021-07-09 11:17:53 +0530

    -- Payment assignment

--- app/Models/PaymentAssignment.php
+++ app/Models/PaymentAssignment.php
@@ -14,11 +14,39 @@ class PaymentAssignment extends Model
         'type'
     ];
     
+    protected $appends = [
+        'assigned_from_amount',
+        'assigned_to_amount',
+    ];
+
     public function assignedBy() {
         return $this->hasOne(User::class, 'id', 'assigned_by');
     }
 
     public function assignedTo() {
         return $this->hasOne(User::class, 'id', 'assigned_to');
+    }
+
+    public function getAssignedFromAmountAttribute() {
+        $user = auth()->user();
+        if($this->assignedTo && $this->assignedTo->id == $user->id) {
+            return [
+                'formated' => number_format($this->amount, '2', ',', '.'),
+                'unformated' => $this->amount
+            ];
+        } else {
+            return null;
+        }
+    }
+    public function getAssignedToAmountAttribute() {
+        $user = auth()->user();
+        if($this->assignedBy && $this->assignedBy->id == $user->id) {
+            return [
+                'formated' => number_format($this->amount, '2', ',', '.'),
+                'unformated' => $this->amount
+            ];
+        } else {
+            return null;
+        }
     }
 }
